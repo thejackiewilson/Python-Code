aaecc775c9a592c6e41c786d7d9ae440e31b1433
Jackie <104796329+thejackiewilson@users.noreply.github.com>
2022-09-27 15:57:09 -0400

    Add files via upload

---
+++ Python Simple Projects/binary_search.py
@@ -0,0 +1,29 @@
+def naive_search(l, target):
+    for i in range (len(l)):
+        if l[i] == target:
+            return i
+        return -1
+    
+def binary_search(l, target, low=None, high=None):
+    if low is None:
+        low = 0
+    if high is None:
+        high = (len(l))-1
+        
+    if high < low:
+        return -1
+        
+    midpoint = (low + high) // 2 
+    
+    if l[midpoint] == target:
+        return midpoint
+    elif target < l[midpoint]:
+        return binary_search(l, target, low, midpoint-1)
+    else:
+        return binary_search(l, target, midpoint+1, high)
+    
+if __name__=='__main__':
+    l = [5, 10, 15]
+    target = 10
+    print(naive_search(l, target))
+    print(binary_search(l, target))
