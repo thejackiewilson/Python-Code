873edbf60952e1b9b8b6f2c51db470eab2c5d637
Jackie <104796329+thejackiewilson@users.noreply.github.com>
2023-01-27 11:10:26 -0500

    Update StockIdeaWithinPython.py

--- Python Trickery/StockIdeaWithinPython.py
+++ Python Trickery/StockIdeaWithinPython.py
@@ -1,37 +1,49 @@
 import yfinance as yf
 import pandas as pd
 import matplotlib.pyplot as plt
-from datetime import date, timedelta, datetime
+from datetime import datetime, timedelta
+import matplotlib.dates as mdates
 
-stock = yf.Ticker("AAPL")
+# Function to calculate moving averages
+def moving_averages(stock, days):
+    end_date = datetime.now()
+    start_date = end_date - timedelta(days=days)
+    hist = stock.history(interval='1d', start=start_date, end=end_date)
+    hist['Close'].plot(figsize=(12,5))
+    plt.xlabel('Date')
+    plt.ylabel('Price')
+    plt.title(f'{days} Day Moving Averages')
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter("%A"))
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter("%b %d"))
+    plt.plot(hist.index, hist['Close'].rolling(window=5).mean(), label='5 Day MA')
+    plt.plot(hist.index, hist['Close'].rolling(window=10).mean(), label='10 Day MA')
+    plt.plot(hist.index, hist['Close'].rolling(window=20).mean(), label='20 Day MA')
+    plt.legend()
+    plt.show()
+
+# Function to calculate success rate
+def success_rate(stock, days):
+    end_date = datetime.now()
+    start_date = end_date - timedelta(days=days)
+    hist = stock.history(interval='1d', start=start_date, end=end_date)
+    current_price = hist['Close'][-1]
+    average_close = hist['Close'].mean()
+    price_change = (current_price - average_close) / average_close * 100
+    if price_change > 0:
+        success = 'Success'
+    else:
+        success = 'Failure'
+    print(f'Over the past {days} days, the stock has had a {price_change:.2f}% change in price. This can be considered a {success}.')
+
+# Get the stock information
+stock = yf.Ticker("GME")
 
 # Get the stock's historical market data
 end_date = datetime.now()
-start_date = end_date - timedelta(days=1)
+start_date = end_date - timedelta(days=365)
 hist = stock.history(interval='1d', start=start_date, end=end_date)
 
-# Convert the data to a pandas DataFrame
-df = pd.DataFrame(hist)
+# Print the moving averages and success rate
+moving_averages(stock, 365)
+success_rate(stock, 365)
 
-# Calculate the stock's average closing price
-average_close = df['Close'].mean()
-
-# Get the stock's current price
-print(hist)
-current_price = hist.iloc[-1]['Close']
-
-# Calculate the stock's price change percentage
-price_change = (current_price - average_close) / average_close * 100
-
-# Print the results
-print("Stock:", stock.info['longName'])
-print("Average Closing Price:", average_close)
-print("Current Price:", current_price)
-print("Price Change: {:.2f}%".format(price_change))
-
-# Plot the stock's closing price over time
-plt.plot(df['Close'])
-plt.xlabel('Year')
-plt.ylabel('Closing Price')
-plt.title(stock.info['longName'] + ' Closing Price')
-plt.show()
