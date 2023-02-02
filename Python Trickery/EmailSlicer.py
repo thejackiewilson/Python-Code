1a275d1df6401aaa539536be5ce8ee8c2e35d387
Jackie <104796329+thejackiewilson@users.noreply.github.com>
2023-02-02 17:32:46 -0500

    Update EmailSlicer.py

--- Python Trickery/EmailSlicer.py
+++ Python Trickery/EmailSlicer.py
@@ -1,21 +1,20 @@
-import string
 import re
 
 def email_slicer(email):
-  if '@' not in email:
-    print("Not Valid Email")
-    return None
-  else:
-    username, domain = email.split('@')
-    if re.match("^[A-Za-z0-9._%+-]+$", username) and re.match("^[A-Za-z0-9.-]+$", domain):
-      print('Username:', username)
-      print('Domain:', domain)
-      return username, domain
-
+    if '@' not in email:
+        print("Not a valid email")
+        return None
     else:
-    
-    print("Invalid characters in email")
-    return None 
-    except ValueError:
-    print("Invalid email address")
-    return None  
+        try:
+            username, domain = email.split('@')
+        except ValueError:
+            print("Invalid email address")
+            return None
+
+        if re.match("^[A-Za-z0-9._%+-]+$", username) and re.match("^[A-Za-z0-9.-]+$", domain):
+            print('Username:', username)
+            print('Domain:', domain)
+            return username, domain
+        else:
+            print("Invalid characters in email")
+            return None
