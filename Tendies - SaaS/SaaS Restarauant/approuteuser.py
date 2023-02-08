fb3ff343a0e3867d9ad04152cf92ffe7ec225f22
Jackie <104796329+thejackiewilson@users.noreply.github.com>
2023-02-07 19:12:28 -0500

    Create approuteuser.py

---
+++ Tendies - SaaS/SaaS Restarauant/approuteuser.py
@@ -0,0 +1,10 @@
+from flask import Flask, redirect, url_for
+
+
+@app.route("/<name>")
+def user(name):
+    return f"Hello {name}!"
+
+@app.route("/admin")
+def admin():
+    return redirect(url_for("user", name="Admin!"))
